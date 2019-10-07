d8d79f6d88d05ba21a0c42dbcdbcc7c5019c9c0d
Michele Cattellani <mcattellani@gmail.com>
2019-10-08 00:34:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const jwt = require('jsonwebtoken');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Origin", ["http://localhost:4200", 'http://localhost:4200/*', 'http://localhost:4200/nuovopaziente', 'http://localhost:4200/login']);  
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Access-Control-Allow-Headers, Content-Type, Accept");
     next();
