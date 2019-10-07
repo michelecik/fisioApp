9937b85f5db762acc816cb74105f2366c74882da
Michele Cattellani <mcattellani@gmail.com>
2019-10-07 20:30:13 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 var app = express();
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); 
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Access-Control-Allow-Headers, Content-Type, Accept");
     next();
   });
