1d8ea54c57ca13d7279ae4714df4b006176b5d43
Michele Cattellani <mcattellani@gmail.com>
2020-01-23 00:34:29 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
 var mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:4321/fisio', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect('mongodb://localhost:27017/fisio', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
         console.log('errorone')
         console.log(err)
